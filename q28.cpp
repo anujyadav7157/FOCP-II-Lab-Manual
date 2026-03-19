3114479a6fb751a1703f784ee51a37ae01279613
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-19 22:15:44 +0530

    Implement even and odd sum calculation

---
+++ q28.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5], evenSum = 0, oddSum = 0;
+
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+        if(arr[i] % 2 == 0)
+            evenSum += arr[i];
+        else
+            oddSum += arr[i];
+    }
+
+    cout << "Even Sum = " << evenSum << endl;
+    cout << "Odd Sum = " << oddSum;
+
+    return 0;
+}
