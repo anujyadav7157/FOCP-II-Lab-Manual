6de59dfbdddb103ff1ab9348c9a1352565e465ef
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:11:14 +0530

    Fix formatting and clean up whitespace in q4.cpp

--- q4.cpp
+++ q4.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int f, c, r;
-
     cout << "Enter 0 for F to C, 1 for C to F: ";
     cin >> r;
-
     if (r == 0)
     {
         cout << "Enter degree in Fahrenheit: ";
@@ -20,6 +17,5 @@ int main()
         cin >> c;
         cout << "Degree in fahrenheit = " << (c * 9.0 / 5) + 32;
     }
-
     return 0;
 }
