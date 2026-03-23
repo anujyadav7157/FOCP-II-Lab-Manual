14d64375135caed83362eeca8c975f6c6286b577
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:10:25 +0530

    Implement temperature conversion functionality
    Replaced the original main function that calculated the area of a circle with a new main function that converts temperatures between Fahrenheit and Celsius.

--- q3.cpp
+++ q3.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int r, area;
-    cout << "Enter radius of the fountain";
-    cin >> r;
-    cout << "radius is:" << r << "area is:" << (3.14 * r * r);
+
+int main() {
+    float temp;
+    int choice;
+
+    cout << "1. Fahrenheit to Celsius\n";
+    cout << "2. Celsius to Fahrenheit\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "Enter temperature in Fahrenheit: ";
+        cin >> temp;
+        cout << "Temperature in Celsius = " << (temp - 32) * 5 / 9;
+    }
+    else if (choice == 2) {
+        cout << "Enter temperature in Celsius: ";
+        cin >> temp;
+        cout << "Temperature in Fahrenheit = " << (temp * 9 / 5) + 32;
+    }
+    else {
+        cout << "Invalid choice";
+    }
+
     return 0;
 }
