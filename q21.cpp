33e4e787c846b12969b72f0817be32233a7b9139
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:26:53 +0530

    Update q21.cpp

--- q21.cpp
+++ q21.cpp
@@ -1,14 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    for(int i =1;i<=5;i++)
-    {
-        for(int j=5;j>=i;j--)
-        {
-            cout<<j;
+int main() {
+    int rows, cols;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= cols; j++) {
+            if (i == 1 || i == rows || j == 1 || j == cols)
+                cout << "*";
+            else
+                cout << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
