589f2d14f901201dc4ff413eb5c68003d38e06d9
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:12:02 +0530

    Update q5.cpp

--- q5.cpp
+++ q5.cpp
@@ -1,10 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int a=10,b=5,c=0;
-    c=a;
-    a=b;
-    b=a;
+int main() {
+    int itemNo, quantity;
+    float unitPrice, amount, discount, finalAmount;
+    cout << "Enter item number: ";
+    cin >> itemNo;
+    cout << "Enter quantity: ";
+    cin >> quantity;
+    cout << "Enter unit price: ";
+    cin >> unitPrice;
+    amount = quantity * unitPrice;
+    discount = amount * 0.20;
+    finalAmount = amount - discount;
+    cout << "Total amount = " << amount << endl;
+    cout << "Discount (20%) = " << discount << endl;
+    cout << "Final bill amount = " << finalAmount << endl;
     return 0;
 }
