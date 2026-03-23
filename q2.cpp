d2e43b313e1c5530ae5dd1c9c126697e89f4fa1b
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:09:43 +0530

    Replace student marks program with circle area calculation

--- q2.cpp
+++ q2.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
-    int Student1, Student2, Student3;
-    cout << "\n ENTER Student 1 MARKS: ";
-    cin >> Student1;
-    cout << "\n ENTER Student 2 MARKS: ";
-    cin >> Student2;
-    cout << "\n ENTER Student 3 MARKS: ";
-    cin >> Student3;
-
-    cout << "\n Student 1 MARKS: " << Student1 << "\n Student 2 MARKS: " << Student2 << "\n Student 3 MARKS: " << Student3 << "\n AVG MARKS: " << (Student1 + Student2 + Student3) / 2;
+int main() {
+    float radius, area;
+    cout << "Enter radius of the fountain: ";
+    cin >> radius;
+    area = 3.14 * radius * radius;
+    cout << "Area of the circle = " << area;
     return 0;
 }
