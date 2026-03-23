a0a654e3779d3af0dd14176ee9f0668ab1ad312b
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:23:15 +0530

    Update q13.cpp

--- q13.cpp
+++ q13.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-int main()
-{
-    float a, b, c, d, r1, r2;
-
+int main() {
+    float a, b, c, D, root1, root2;
     cout << "Enter coefficients a, b, c: ";
     cin >> a >> b >> c;
-
-    d = b * b - 4 * a * c;
-
-    if (d > 0)
-    {
-        r1 = (-b + sqrt(d)) / (2 * a);
-        r2 = (-b - sqrt(d)) / (2 * a);
-        cout << "Root 1 = " << r1 << endl;
-        cout << "Root 2 = " << r2 << endl;
+    D = b*b - 4*a*c;
+    if (D > 0) {
+        root1 = (-b + sqrt(D)) / (2*a);
+        root2 = (-b - sqrt(D)) / (2*a);
+        cout << "Roots are real and different:\n";
+        cout << "Root1 = " << root1 << "\nRoot2 = " << root2;
     }
-    else if (d == 0)
-    {
-        r1 = -b / (2 * a);
-        cout << "Roots are real and equal\n";
-        cout << "Root = " << r1 << endl;
+    else if (D == 0) {
+        root1 = -b / (2*a);
+        cout << "Roots are real and equal:\n";
+        cout << "Root = " << root1;
     }
-    else
-    {
+    else {
         cout << "Roots are complex (imaginary)";
     }
-
     return 0;
 }
