8ac22f8f7892d120bc2bae4d04175125f517a42d
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:12:43 +0530

    Fix formatting and clean up whitespace in q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     float temp;
     int choice;
-
     cout << "1. Fahrenheit to Celsius\n";
     cout << "2. Celsius to Fahrenheit\n";
     cout << "Enter your choice: ";
     cin >> choice;
-
     if (choice == 1) {
         cout << "Enter temperature in Fahrenheit: ";
         cin >> temp;
@@ -23,6 +20,5 @@ int main() {
     else {
         cout << "Invalid choice";
     }
-
     return 0;
 }
