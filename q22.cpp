8e951332c564e072872842338cf4791857ebbda1
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:25:45 +0530

    Update q22.cpp

--- q22.cpp
+++ q22.cpp
@@ -1,17 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    for(int i=0;i<=6;i++)
-    {
-        for(int j=0;j<=3;j++)
-        {
-                if((i==1||i==2||i==3||i==4||i==5)&&(j==1||j==2))
-                    cout<<" ";
-                else
-                    cout<<"*";
+int main() {
+    int n;
+    cout << "Enter number of rows (half diamond): ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int s = 1; s <= n - i; s++)
+            cout << " ";
+        for (int j = 1; j <= 2*i - 1; j++) {
+            if (j == 1 || j == 2*i - 1)
+                cout << "*";
+            else
+                cout << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    
+    for (int i = n - 1; i >= 1; i--) {
+        for (int s = 1; s <= n - i; s++)
+            cout << " ";
+        for (int j = 1; j <= 2*i - 1; j++) {
+            if (j == 1 || j == 2*i - 1)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
