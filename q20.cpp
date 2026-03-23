896a74deb68f7d6abfe79152d9dc003687e2901d
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:28:43 +0530

    Update q20.cpp

--- q20.cpp
+++ q20.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    for(int i=1;i<=3;i++)
-    {
-        for(int j=1;j<=5;j++)
-        {
-            cout<<j;
+
+int main() {
+    int rows = 3, cols = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= cols; j++) {
+            cout << j << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
+
     return 0;
 }
