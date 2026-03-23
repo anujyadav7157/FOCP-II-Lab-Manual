a20bbac7cbf62304853ce2161b872d2fc6fa5976
anujyadav7157 <anujyadav0914@gmail.com>
2026-03-23 19:08:35 +0530

    Refactor main function to calculate average marks

--- q1.cpp
+++ q1.cpp
@@ -1,12 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int a1,a2;
-    cout << "Enter a1 amount";
-    cin >> a1;
-    cout << "\nEnter a2 amount ";
-    cin >> a2;
-    cout << "\na1 amount:" << a1 << "\na2 amount" << a2 << "\ntotal amount="<<a1+a2;
+int main() {
+    float m1, m2, m3, average;
+    cout << "Enter marks of three students: ";
+    cin >> m1 >> m2 >> m3;
+    average = (m1 + m2 + m3) / 3;
+    cout << "Average marks = " << average;
     return 0;
 }
