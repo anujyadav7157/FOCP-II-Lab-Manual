3cb269daabc5fc8ec4e1a8d76c1c3fba2276eea0
anujyadav7157 <anujyadav0914@gmail.com>
2026-05-09 18:36:43 +0530

    Add files via upload

---
+++ classq1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    string name;
+    int marks;
+
+public:
+    Student(string n, int m) {
+        name = n;
+        marks = m;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    Student s("Anuj", 85);
+    s.display();
+    return 0;
+}
